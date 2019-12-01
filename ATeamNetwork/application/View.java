0633343c0336ef1f6702b93de0d2d2f4276db417
snowtothemax <56100786+snowtothemax@users.noreply.github.com>
2019-11-30 18:02:41 -0600

    First Pane
    Creates the first pane for the GUI

--- ATeamNetwork/application/View.java
+++ ATeamNetwork/application/View.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -24,7 +26,7 @@ public class View extends Application {
 
 	private static final int WINDOW_WIDTH = 600;
 	private static final int WINDOW_HEIGHT = 600;
-	private static final String APP_TITLE = "CS400 MyFirstJavaFX";
+	private static final String APP_TITLE = "Welcome!";
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -37,14 +39,9 @@ public class View extends Application {
 			vbox.getChildren().add(new Label("hello " + arg));
 		}
 
-		// Main layout is Border Pane example (top,left,center,right,bottom)
-		BorderPane root = new BorderPane();
-
-		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-
 		// Add the stuff and set the primary stage
 		primaryStage.setTitle(APP_TITLE);
-		primaryStage.setScene(mainScene);
+		primaryStage.setScene(getFirstScene());
 		primaryStage.show();
 	}
 
@@ -58,18 +55,75 @@ public class View extends Application {
 	/**
 	 * Creates the mainSceneBorderPane with functional buttons, etc
 	 */
-	private static Scene getMainScene() {
+	private static Scene getFirstScene() {
 
 		// Main BorderPane for the login screen
 		BorderPane root = new BorderPane();
 
 		// Horizontal box to be but at the top of the application
 		HBox top = new HBox();
+		top.setPrefHeight(WINDOW_HEIGHT/3);
 
 		// Creates a Button that goes in the top center of the application for Central
 		// User Options
 		Button ctrlUsrOp = new Button("Central User Options");
-		top.
+		
+		//sets the position of the button ctrlUsrOp
+		ctrlUsrOp.setTranslateX(WINDOW_WIDTH*3/8);
+		ctrlUsrOp.setTranslateY(WINDOW_HEIGHT/12);
+		top.getChildren().add(ctrlUsrOp);
 
+		// sets the Hbox "top" to the top of the main BoderPane
+		root.setTop(top);
+
+		// creates a Vbox for both the left and right sides of the screen
+		VBox left = new VBox();
+		VBox right = new VBox();
+		left.setPrefHeight(WINDOW_HEIGHT/3);
+		left.setPrefWidth(WINDOW_WIDTH/2);
+		right.setPrefHeight(WINDOW_HEIGHT/3);
+		right.setPrefWidth(WINDOW_WIDTH/2);
+
+		// Buttons to be added to the left side of the scene
+		Button uploadNtwrkFile = new Button("Upload Network File");// used to upload network files
+		Button addNewUser = new Button("Add New User"); // used to add a new user
+
+		// TextField to be added to the left side of the scene.
+		TextField user1 = new TextField("Enter User 1...");
+
+		// Adds all the above nodes to the VBox "left"
+		left.getChildren().addAll(uploadNtwrkFile, addNewUser, user1);
+
+		// Buttons to be added to the right side of the scene.
+		Button exportNtwrkFile = new Button("Export Network File");
+		Button viewNetwork = new Button("View Network");
+
+		// TextField to be added to the right side of the scene. This input and the
+		// input from "user1" will have the option of either adding a friendship
+		// between the two or removing a friendship between the two (If they exist of
+		// course).
+		TextField user2 = new TextField("Enter User 2...");
+
+		// adds correct nodes to each vbox.
+		right.getChildren().addAll(exportNtwrkFile, viewNetwork, user2);
+
+		// The box to be set as the bottom of the screen.
+		HBox bottom = new HBox();
+		// Buttons to be added to the HBox "bottom"
+		Button addFriend = new Button("Add Friendship");// When functional, this should add an edge between both
+														// friends input in the textFields user1 and user2
+		Button removeFriend = new Button("Remove Friendship");// ^^ but remove the friendship
+
+		// adds the buttons to the HBox bottom.
+		bottom.getChildren().addAll(addFriend, removeFriend);
+
+		// Aligns all the boxes to their specified regions in the Main BorderPane
+		// "root".
+		root.setTop(top);
+		root.setLeft(left);
+		root.setRight(right);
+		root.setBottom(bottom);
+
+		return new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 }
