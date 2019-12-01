273881577b112edae7518b90729e3d18f8efc01d
snowtothemax <56100786+snowtothemax@users.noreply.github.com>
2019-12-01 15:10:52 -0600

    Finished First Scene
    Initializes the first scene view for the application. The plan from here is to use the controller to access each

--- ATeamNetwork/application/View.java
+++ ATeamNetwork/application/View.java
@@ -62,15 +62,15 @@ public class View extends Application {
 
 		// Horizontal box to be but at the top of the application
 		HBox top = new HBox();
-		top.setPrefHeight(WINDOW_HEIGHT/3);
+		top.setPrefHeight(WINDOW_HEIGHT / 4);
 
 		// Creates a Button that goes in the top center of the application for Central
 		// User Options
 		Button ctrlUsrOp = new Button("Central User Options");
-		
-		//sets the position of the button ctrlUsrOp
-		ctrlUsrOp.setTranslateX(WINDOW_WIDTH*3/8);
-		ctrlUsrOp.setTranslateY(WINDOW_HEIGHT/12);
+
+		// sets the position of the button ctrlUsrOp
+		ctrlUsrOp.setTranslateX(WINDOW_WIDTH*5/12);
+		ctrlUsrOp.setTranslateY(WINDOW_HEIGHT / 12);
 		top.getChildren().add(ctrlUsrOp);
 
 		// sets the Hbox "top" to the top of the main BoderPane
@@ -79,10 +79,10 @@ public class View extends Application {
 		// creates a Vbox for both the left and right sides of the screen
 		VBox left = new VBox();
 		VBox right = new VBox();
-		left.setPrefHeight(WINDOW_HEIGHT/3);
-		left.setPrefWidth(WINDOW_WIDTH/2);
-		right.setPrefHeight(WINDOW_HEIGHT/3);
-		right.setPrefWidth(WINDOW_WIDTH/2);
+		left.setPrefHeight(WINDOW_HEIGHT / 2);
+		left.setPrefWidth(WINDOW_WIDTH / 2);
+		right.setPrefHeight(WINDOW_HEIGHT / 2);
+		right.setPrefWidth(WINDOW_WIDTH / 2);
 
 		// Buttons to be added to the left side of the scene
 		Button uploadNtwrkFile = new Button("Upload Network File");// used to upload network files
@@ -91,8 +91,20 @@ public class View extends Application {
 		// TextField to be added to the left side of the scene.
 		TextField user1 = new TextField("Enter User 1...");
 
-		// Adds all the above nodes to the VBox "left"
+		// Adds all the above nodes to the VBox "left" and also positions them to their
+		// correct areas on the screen
+		//Translates the uploadNtwrkFile button to its position in the screen
+		uploadNtwrkFile.setTranslateX(WINDOW_WIDTH/5);
+		
+		//Translates the addNewUser button to its correct position on the screen
+		addNewUser.setTranslateX(WINDOW_WIDTH/5);
+		addNewUser.setTranslateY(WINDOW_HEIGHT/10);
+		
+		//Translates the user1 TextField to its correct position on the screen
+		user1.setTranslateY(WINDOW_HEIGHT/4);
 		left.getChildren().addAll(uploadNtwrkFile, addNewUser, user1);
+		
+		
 
 		// Buttons to be added to the right side of the scene.
 		Button exportNtwrkFile = new Button("Export Network File");
@@ -103,17 +115,45 @@ public class View extends Application {
 		// between the two or removing a friendship between the two (If they exist of
 		// course).
 		TextField user2 = new TextField("Enter User 2...");
-
+		
+		
+		
+		//Positions the above buttons to their correct positions on the screen
+		
+		//Positions the exportNtwrkFile button
+		exportNtwrkFile.setTranslateX(WINDOW_WIDTH/8);
+		
+		//Positons the viewNetwork button
+		viewNetwork.setTranslateX(WINDOW_WIDTH/8);
+		viewNetwork.setTranslateY(WINDOW_HEIGHT/10);
+		
+		//positions the user2 TextField to its correct position
+		user2.setTranslateY(WINDOW_HEIGHT/4);
+		
 		// adds correct nodes to each vbox.
 		right.getChildren().addAll(exportNtwrkFile, viewNetwork, user2);
+		
+		
 
 		// The box to be set as the bottom of the screen.
-		HBox bottom = new HBox();
+		VBox bottom = new VBox();
+		bottom.setPrefHeight(WINDOW_HEIGHT/4);
+		bottom.setSpacing(30);
+		
 		// Buttons to be added to the HBox "bottom"
 		Button addFriend = new Button("Add Friendship");// When functional, this should add an edge between both
 														// friends input in the textFields user1 and user2
 		Button removeFriend = new Button("Remove Friendship");// ^^ but remove the friendship
 
+		//Positions the bottom buttons correctly onto the scene
+		
+		//Sets the addFriend button to the center of the screen
+		addFriend.setTranslateX(WINDOW_WIDTH*5/12);
+		
+		//sets the removeFriend button to the center of the screen but below the addFriend button
+		removeFriend.setTranslateX(WINDOW_WIDTH*5/12);
+		//removeFriend.setTranslateY(WINDOW_HEIGHT/4);
+		
 		// adds the buttons to the HBox bottom.
 		bottom.getChildren().addAll(addFriend, removeFriend);
 
